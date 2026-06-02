164beb47c7d2244acaa7f1a509f54160bc413f1b
Sigma-Demo <rudra610prakash@gmail.com>
2026-06-02 20:31:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+let n = 0;
+
+function changeSlider() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+
+changeSlider();
+
+prev_btn.addEventListener('click', () => {
+    if (n > 0) {
+        n--;
+    } else {
+        n = imgs.length - 1;
+    }
+    changeSlider();
+});
+
+next_btn.addEventListener('click', () => {
+    if (n < imgs.length -1) {
+        n++;
+    } else {
+        n = 0;
+    }
+    changeSlider();
+});
+
+const scrollContainer = document.querySelectorAll('.products');
+for (const item of scrollContainer) {
+    item.addEventListener('wheel'  ,  (evt) => {
+          evt.preventDefault();
+          item.scrollLeft += evt.deltaY;
+    });
+}
